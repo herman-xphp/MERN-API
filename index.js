b41358c734ec5fe503880eef7e68e264fd01038d
herman-xphp <hermanxphp@gmail.com>
2020-11-15 13:33:45 +0800

    membuat dan memahami basic router, request dan response di NodeJS

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
+
+router.use('/products', (req, res, next) => {
+  res.json({name: "Herman", email: "hermanxphp@gmail.com"});
+  next();
+})
+
+router.use('/price', (req, res, next) => {
+  res.json({price: 1500000});
+  next();
+})
+
+router.delete('/customers', (req, res, next) => {
+  res.json({title: "Customer"});
+  next();
+})
+app.use('/', router);
 
 app.listen(8080);
 console.log("Running on localhost:8080");
