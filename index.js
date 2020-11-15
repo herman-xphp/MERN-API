1aad3020f4ab2154386aca8eb820b6e7d43d76dc
herman-xphp <hermanxphp@gmail.com>
2020-11-15 13:51:54 +0800

    Restructure Projects NodeJS_ExpressJS

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 const express = require('express');
 
 const app = express();
-const router = express.Router();
+const productRoutes = require('./src/routes/products');
 
-router.use('/products', (req, res, next) => {
-  res.json({name: "Herman", email: "hermanxphp@gmail.com"});
-  next();
-})
-
-router.use('/price', (req, res, next) => {
-  res.json({price: 1500000});
-  next();
-})
-
-router.delete('/customers', (req, res, next) => {
-  res.json({title: "Customer"});
-  next();
-})
-app.use('/', router);
+app.use('/', productRoutes);
 
 app.listen(8080);
 console.log("Running on localhost:8080");
